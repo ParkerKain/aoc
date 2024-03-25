5cca2bfea3558c2385031eddff25da626507e448
Parker Kain <parkerjkain@gmail.com>
2024-03-25 19:00:42 -0400

    day 2!

--- 2020/rust/day_2/src/main.rs
+++ 2020/rust/day_2/src/main.rs
@@ -1,47 +1,85 @@
-use std::{fs, vec};
+use std::fs;
 
 use regex::Regex;
 
 fn main() {
     println!("Beginning Day 2");
     let f = fs::read_to_string("input").unwrap();
-    // let input: Vec<&str> = f.split("\n").collect();
-    let input: Vec<&str> = vec!["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"];
-    part_one(input);
-    part_two();
+    let input: Vec<&str> = f.split('\n').collect();
+    // let input: Vec<&str> = vec!["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"];
+    // part_one(input);
+    part_two(input);
 }
 
 #[derive(Debug)]
 struct Input<'a> {
-    min: u16,
-    max: u16,
+    min: usize,
+    max: usize,
     letter: char,
     string: &'a str,
+}
+
+impl<'a> Input<'a> {
+    fn solve_pt1(&self) -> u16 {
+        let mut curr_occurances = 0;
+        for c in self.string.chars() {
+            if c == self.letter {
+                curr_occurances += 1;
+                if curr_occurances > self.max {
+                    return 0;
+                }
+            }
+        }
+        match curr_occurances >= self.min {
+            false => 0,
+            true => 1,
+        }
+    }
+
+    fn solve_pt2(&self) -> u16 {
+        let match_min: u16 = (self.string.chars().nth(self.min - 1).unwrap() == self.letter) as u16;
+        let match_max: u16 = (self.string.chars().nth(self.max - 1).unwrap() == self.letter) as u16;
+
+        (match_min + match_max) % 2
+    }
 }
 
 fn _parse_input_line(line: &str) -> Input {
     let re = Regex::new(r"-| |:").unwrap();
     let fields: Vec<&str> = re.split(line).collect();
 
-    let min: u16 = fields[0].parse().unwrap();
-    let max: u16 = fields[1].parse().unwrap();
+    let min: usize = fields[0].parse().unwrap();
+    let max: usize = fields[1].parse().unwrap();
     let letter: char = fields[2].chars().next().unwrap();
     let string = fields[4];
-    let cleaned_input: Input = Input {
+    Input {
         min,
         max,
         letter,
         string,
-    };
-
-    return cleaned_input;
-}
-
-fn part_one(input: Vec<&str>) {
-    for row in input {
-        let cleaned_input: Input = _parse_input_line(row);
-        println!("{:?}", cleaned_input);
     }
 }
 
-fn part_two() {}
+fn part_one(input: Vec<&str>) {
+    let mut answer: u16 = 0;
+    for row in input {
+        if row.is_empty() {
+            break;
+        }
+        let cleaned_input: Input = _parse_input_line(row);
+        answer += cleaned_input.solve_pt1();
+    }
+    println!("Final Part One Answer: {}", answer);
+}
+
+fn part_two(input: Vec<&str>) {
+    let mut answer: u16 = 0;
+    for row in input {
+        if row.is_empty() {
+            break;
+        }
+        let cleaned_input: Input = _parse_input_line(row);
+        answer += cleaned_input.solve_pt2();
+    }
+    println!("Final Part Two Answer: {}", answer);
+}
